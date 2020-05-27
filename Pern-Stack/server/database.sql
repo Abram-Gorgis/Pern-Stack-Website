ddf570292f48a0ec756d6a9a2dc62518dfefa323
Abram-Gorgis <39226708+Abram-Gorgis@users.noreply.github.com>
2020-05-27 14:13:03 -0500

    Update database.sql

--- Pern-Stack/server/database.sql
+++ Pern-Stack/server/database.sql
@@ -5,22 +5,6 @@ CREATE TABLE highscores(
     score int
 );
 
-select * from highscores
-where username = 'kim' in (SELECT Rank() OVER(ORDER by score desc) rank,* FROM highscores ORDER BY score DESC);
 
-SELECT Rank() OVER(ORDER by score desc) rank, * FROM highscores where username = $1 in (Select username From highscores
 
-WITH highscores AS (
-	SELECT 
-		RANK() OVER (
-			ORDER BY score DESC) rank,
-            *
-	FROM 
-		highscores
-
-)
-SELECT 
-	*
-FROM 
-	highscores 
-WHERE username='kim';	
+WITH highscore AS (SELECT RANK() OVER (ORDER BY score DESC) rank, * FROM highscores) SELECT * FROM highscore WHERE username='kim';	
